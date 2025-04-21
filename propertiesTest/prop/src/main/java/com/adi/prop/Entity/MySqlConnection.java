1590a4ebc9d29e56f6d0a6f68499a2b4b8d10076
[Aditya <[www.aditya117@gmail.com]>
2025-04-21 12:55:06 +0530

    Profiling

--- propertiesTest/prop/src/main/java/com/adi/prop/Entity/MySqlConnection.java
+++ propertiesTest/prop/src/main/java/com/adi/prop/Entity/MySqlConnection.java
@@ -1,12 +1,29 @@
 package com.adi.prop.Entity;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(prefix = "sqlconnection" , value = "enabled" , havingValue = "true" , matchIfMissing = false)
+// @ConditionalOnProperty(prefix = "sqlconnection" , value = "enabled" , havingValue = "true" , matchIfMissing = false)
+@Profile("qa")
 public class MySqlConnection {
+
     MySqlConnection(){
         System.out.println("Initialization of MySqlBean");
     }
+
+    @Value("${username}")
+    String username;
+
+    @Value("${password}")
+    String password;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("USurname Mysql : "+username + " Password : "+ password);
+    }
+
 }
--- propertiesTest/prop/src/main/java/com/adi/prop/Entity/NoSqlConnection.java
+++ propertiesTest/prop/src/main/java/com/adi/prop/Entity/NoSqlConnection.java
@@ -1,12 +1,27 @@
 package com.adi.prop.Entity;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(prefix = "nosqlconnection" , value = "enabled" , havingValue = "true" , matchIfMissing = false)
+// @ConditionalOnProperty(prefix = "nosqlconnection" , value = "enabled" , havingValue = "true" , matchIfMissing = false)
+@Profile("dev")
 public class NoSqlConnection {
     NoSqlConnection(){
         System.out.println("Initialization of NoSql Bean");
+    }
+
+    @Value("${username}")
+    String username;
+
+    @Value("${password}")
+    String password;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("USername Nosql : "+username + " Password : "+ password);
     }
 }
